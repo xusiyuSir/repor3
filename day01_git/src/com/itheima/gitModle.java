2a62cd1909484ecaee83a8d24218a5c78834ecfb
xusiyu <xusiyu@qq.com>
2019-03-21 21:13:55 +0800

    添加到本地

---
+++ day01_git/src/com/itheima/gitModle.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class gitModle {
+    public static void main(String[] args) {
+        System.out.println("hello.....");
+        System.out.println("hello.....");
+        System.out.println("hello.....");
+        System.out.println("hello.....");
+        System.out.println("hello.....");
+    }
+}
